3f4dc3bff46108f4237bce80b7701f14bddc2e3f
unrealwork <unrealwork@gmail.com>
2017-11-23 00:13:54 +0300

    revert webpack production config

--- conf/webpack.prod.js
+++ conf/webpack.prod.js
@@ -5,12 +5,24 @@ const CommonConfig = require('./webpack.common.js');
 module.exports = Merge(CommonConfig, {
     plugins: [
         new webpack.LoaderOptionsPlugin({
-            minimize: false,
+            minimize: true,
             debug: false
         }),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
-        })]
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        })
+    ]
 });
